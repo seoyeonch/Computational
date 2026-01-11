214cc891aae119532a076831d62b859a07fb57b4
S-Chae <seoyeonchae03@gmail.com>
2026-01-12 00:05:05 +0900

    Create run_band.sh

---
+++ 01_Electronic_Structure_DFT/WSe2_monolayer_2H_vs_1T/calc/2H/06_band/run_band.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#===============================================================================
+#SBATCH -J 2H-WSe2_band         # job name
+#SBATCH -o 2H-WSe2_band.o%j     # output and error file name (%j expands to jobID)
+#-----------------------------
+#SBATCH --partition=ivybrid  # queue name: ivybrid (broadwe skylake emerald)
+#SBATCH --nodes=1            # number of nodes
+#SBATCH --ntasks-per-node=12 # cores per node
+#SBATCH --exclusive
+#===============================================================================
+VASP642avx1="/home/VASP/vasp4us_hk/vasp.6.4.2.avx1.std.fioG13.6.x"
+VASP642avx1g="/home/VASP/vasp4us_hk/vasp.6.4.2.avx1.gam.fioG13.6.x"
+VASP=${VASP642avx1}
+#VASP=${VASP642avx1g}
+# ------------------------------------------------------------------------------
+module purge
+module add compiler/2023.1.0
+module add mkl/2023.1.0
+module add mpi/2021.12.1
+module add VASP/basic_tools
+#===============================================================================
+cp cont.eq POSCAR; cp CHG.eq CHGCAR
+cp INCAR.band INCAR; cp KPT.band KPOINTS
+
+mpirun -np $SLURM_NTASKS $VASP > stdout
+
+mv stdout std.band; mv OUTCAR out.band
+mv PROCAR pro.band; mv EIGENVAL eig.band
