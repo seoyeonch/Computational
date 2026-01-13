b33729696e829c9095b0ebd8b5c614a106348e58
S-Chae <seoyeonchae03@gmail.com>
2026-01-13 23:08:02 +0900

    Update run_econv.sh

--- 01_Electronic_Structure_DFT/Si_bulk/calc/02_econv/run_econv.sh
+++ 01_Electronic_Structure_DFT/Si_bulk/calc/02_econv/run_econv.sh
@@ -5,7 +5,7 @@ for ECUT in 200 225 250 275 300 325 350 375 400 425 450 475 500 525 550 \
             575 600 650 675 700 725 750 775 800 825 850 875 900
 do
 # for ((ECUT=200; ECUT<=900; ECUT=ECUT+25)); do
-  sed "s/ENCUT=.*/ENCUT=$E/" INCAR.template > INCAR
+  sed "s/ENCUT=.*/ENCUT=$E/" INCAR.ecut > INCAR
   
   mpirun -np $NPROC vasp_std > stdout
   
