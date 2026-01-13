8ba8bfb5187e40fb7c1da28dabf34a6700903879
S-Chae <seoyeonchae03@gmail.com>
2026-01-13 22:54:19 +0900

    Update run_kconv.sh

--- 01_Electronic_Structure_DFT/WSe2_monolayer_2H_vs_1T/calc/2H/03_kconv/run_kconv.sh
+++ 01_Electronic_Structure_DFT/WSe2_monolayer_2H_vs_1T/calc/2H/03_kconv/run_kconv.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
-#==================================================================================
-#SBATCH -J 2H-WSe2_kconv        # job name
-#SBATCH -o 2H-WSe2_kconv.o%j    # output and error file name (%j expands to jobID)
-#-----------------------------
-#SBATCH --partition=ivybrid  # queue name: ivybrid (broadwe skylake emerald)
-#SBATCH --nodes=1            # number of nodes
-#SBATCH --ntasks-per-node=12 # cores per node
-#SBATCH --exclusive
-#==================================================================================
-VASP642avx1="/home/VASP/vasp4us_hk/vasp.6.4.2.avx1.std.fioG13.6.x"
-VASP642avx1g="/home/VASP/vasp4us_hk/vasp.6.4.2.avx1.gam.fioG13.6.x"
-VASP=${VASP642avx1}
-#VASP=${VASP642avx1g}
-# ---------------------------------------------------------------------------------
-module purge
-module add compiler/2023.1.0
-module add mkl/2023.1.0
-module add mpi/2021.12.1
-module add VASP/basic_tools
-#==================================================================================
+# k-point convergence test
+
 for (( k = 4; k <= 40; k = k + 4 )); do
   kpt=$k
   if (( k < 10 )); then
@@ -35,7 +17,7 @@ ${kpt} ${kpt} 1
 #---
   cp KPT.$kpt KPOINTS
 
-  mpirun -np $SLURM_NTASKS $VASP > stdout
+  mpirun -np $NPROC vasp_std > stdout
 
   mv OUTCAR out.k$kpt
   mv stdout std.k$kpt
