250717922a62433507d9641e72bfc08ed116fecb
S-Chae <seoyeonchae03@gmail.com>
2026-01-13 23:04:29 +0900

    Create run_kconv.sh

---
+++ 01_Electronic_Structure_DFT/Si_bulk/calc/03_kconv/run_kconv.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# k-point convergence test
+
+for (( k = 4; k <= 40; k = k + 4 )); do
+  kpt=$k
+  if (( k < 10 )); then
+     kpt=0$k
+  fi
+#--- create KPT.$kpt
+cat << ! > KPT.$kpt
+Gamma-${kpt}x${kpt}
+0
+Gamma Monkhorst-Pack
+${kpt} ${kpt} 1
+0 0 0
+!
+#---
+  cp KPT.$kpt KPOINTS
+
+  mpirun -np $NPROC vasp_std > stdout
+
+  mv OUTCAR out.k$kpt
+  mv stdout std.k$kpt
+done
