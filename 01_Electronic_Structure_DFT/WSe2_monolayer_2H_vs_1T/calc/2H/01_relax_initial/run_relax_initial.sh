f53a4ebb715437e455526df013e37d3f19b878c1
S-Chae <seoyeonchae03@gmail.com>
2026-01-13 22:44:39 +0900

    Update run_relax_initial.sh

--- 01_Electronic_Structure_DFT/WSe2_monolayer_2H_vs_1T/calc/2H/01_relax_initial/run_relax_initial.sh
+++ 01_Electronic_Structure_DFT/WSe2_monolayer_2H_vs_1T/calc/2H/01_relax_initial/run_relax_initial.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
-#==================================================================================
-#SBATCH -J 2H-WSe2_relax_init          # job name
-#SBATCH -o 2H-WSe2_relax_init.o%j      # output and error file name (%j expands to jobID)
-#-----------------------------
-#SBATCH --partition=ivybrid  # queue name: ivybrid (broadwe skylake emerald)
-#SBATCH --nodes=1            # number of nodes
-#SBATCH --ntasks-per-node=12 # cores per node
-#SBATCH --exclusive
-#==================================================================================
-VASP642avx1="/home/VASP/vasp4us_hk/vasp.6.4.2.avx1.std.fioG13.6.x"
-VASP642avx1g="/home/VASP/vasp4us_hk/vasp.6.4.2.avx1.gam.fioG13.6.x"
-VASP=${VASP642avx1}
-#VASP=${VASP642avx1g}
-# ---------------------------------------------------------------------------------
-module purge
-module add compiler/2023.1.0
-module add mkl/2023.1.0
-module add mpi/2021.12.1
-module add VASP/basic_tools
-#==================================================================================
-mpirun -np $SLURM_NTASKS $VASP > stdout
+# Generic VASP run script (initial relaxation)
+
+mpirun -np $NPROC vasp_std > stdout
