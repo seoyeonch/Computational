0258542a3b15f1f049ae7ade400a0ae2aa662d9e
S-Chae <seoyeonchae03@gmail.com>
2026-01-14 13:44:56 +0900

    Create run_relax.sh

---
+++ 01_Electronic_Structure_DFT/Si_bulk/calc/relax/run_relax.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Step 1: Variable-cell relaxation
+cp INCAR_vcsv INCAR
+cp POSCAR_initial POSCAR
+vasp_std
+
+# Step 2: Final ionic relaxation at fixed cell
+cp CONTCAR POSCAR
+cp INCAR_rlx INCAR
+vasp_std
