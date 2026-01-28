14a3b244bbd9a065f3dc72177f797fd25e6069c2
S-Chae <seoyeonchae03@gmail.com>
2026-01-28 13:22:30 +0900

    Update run_relax.sh

--- 01_Electronic_Structure_DFT/Si_bulk/calc/02_relaxation/run_relax.sh
+++ 01_Electronic_Structure_DFT/Si_bulk/calc/02_relaxation/run_relax.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Step 1: Variable-cell relaxation
-cp INCAR_vcsv INCAR
+cp INCAR.vcsv INCAR
 cp POSCAR.exp POSCAR
 vasp_std
 
 # Step 2: Final ionic relaxation at fixed cell
 cp CONTCAR POSCAR
-cp INCAR_rlx INCAR
+cp INCAR.rlxeq INCAR
 vasp_std
